cfcd928756983b70dc2ccdda23de2ed61a1e3bab
proomguy <mattz100bc@gmail.com>
2023-03-19 23:49:49 +0300

    Animation coding in the login screen

---
+++ lib/LoginPage/login_screen.dart
@@ -0,0 +1,60 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../Services/global_variables.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> with TickerProviderStateMixin{
+
+  late Animation<double> _animation;
+  late AnimationController _animationController;
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 20));
+    _animation = CurvedAnimation(parent: _animationController, curve: Curves.linear)
+      ..addListener(() { })
+      ..addStatusListener((animationStatus) {
+        if(animationStatus == AnimationStatus.completed){
+          _animationController.reset();
+          _animationController.forward();
+        }
+      });
+    _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          CachedNetworkImage(
+            imageUrl: loginUrlImage,
+            placeholder: (context, url) => Image.asset(
+              'assets/images/wallpaper.jpg',
+              fit: BoxFit.fill,
+            ),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+            alignment: FractionalOffset(_animation.value, 0),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,2 @@
+
+String loginUrlImage = 'https://media.istockphoto.com/id/1181546520/photo/futuristic-skyscrapers-at-dusk.jpg?s=612x612&w=0&k=20&c=hEDFOltZtmaaQoyDySHu8HLvaPd8XZ5JYKWKoGaPwyk=';

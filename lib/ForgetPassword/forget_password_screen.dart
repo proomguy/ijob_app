d2fa92b91f8cde0d1de201f42f7fdabe3c8118b2
proomguy <mattz100bc@gmail.com>
2023-03-20 02:10:37 +0300

    Improvements on the Log in page

---
+++ lib/ForgetPassword/forget_password_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ForgetPasswordScreen extends StatefulWidget {
+
+
+  @override
+  State<ForgetPasswordScreen> createState() => _ForgetPasswordScreenState();
+}
+
+class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/Services/global_methods.dart
@@ -0,0 +1,56 @@
+
+import 'package:flutter/material.dart';
+
+class GlobalMethod{
+  static void showErrorDialog({required String error, required BuildContext ctx}){
+
+    showDialog(
+        context: ctx,
+        builder: (context){
+          return AlertDialog(
+            title: const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.logout,
+                    color: Colors.deepOrange,
+                    size: 35,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Error Occcured',
+
+                  ),
+                ),
+              ],
+            ),
+            content: Text(
+              error,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontStyle: FontStyle.italic
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: (){
+                    Navigator.canPop(context) ? Navigator.pop(context) : null;
+                  },
+                  child: const Text(
+                    'Okay',
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                    ),
+                  ),
+              ),
+            ],
+          );
+        }
+    );
+
+  }
+}

226cd9974eda8029b09dba28ba5767f15c28af11
proomguy <mattz100bc@gmail.com>
2023-03-27 22:20:28 +0300

    Implementing Job details screen

--- lib/Jobs/job_details_screen.dart
+++ lib/Jobs/job_details_screen.dart
@@ -1,14 +1,117 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:ijob_app/Jobs/jobs_screen.dart';
 
 class JobDetailsScreen extends StatefulWidget {
+
+  final String uploadedBy;
+  final String jobID;
+
+  const JobDetailsScreen({
+    required this.uploadedBy,
+    required this.jobID,
+});
 
   @override
   State<JobDetailsScreen> createState() => _JobDetailsScreenState();
 }
 
 class _JobDetailsScreenState extends State<JobDetailsScreen> {
+
+  String? authorName;
+  String? userImageUrl;
+  String? jobCategory;
+  String? jobDescription;
+  String? jobTitle;
+  bool? jobRecruitment;
+  Timestamp? postedDateTimeStamp;
+  Timestamp? deadlineDateTimeStamp;
+  String? postedDate;
+  String? deadlineDate;
+  String? locationCompany = '';
+  String? emailCompany = '';
+  int applicants = 0;
+  bool isDeadlineAvailable = false;
+
+  void getJobData() async{
+
+    final DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(widget.uploadedBy).get();
+
+    if(userDoc == null){
+      return;
+    }
+    else{
+      setState(() {
+        authorName = userDoc.get('name');
+        userImageUrl = userDoc.get('userImage');
+      });
+    }
+
+    final DocumentSnapshot jobDatabase = await FirebaseFirestore.instance.collection('jobs').doc(widget.jobID).get();
+
+    if(jobDatabase == null){
+      return;
+    }
+    else{
+      setState(() {
+        jobTitle = jobDatabase.get('jobTitle');
+        jobDescription = jobDatabase.get('jobDescription');
+        jobRecruitment = jobDatabase.get('jobRecruitment');
+        emailCompany = jobDatabase.get('email');
+        locationCompany = jobDatabase.get('location');
+        applicants = jobDatabase.get('applicants');
+        postedDateTimeStamp = jobDatabase.get('createdAt');
+        deadlineDateTimeStamp = jobDatabase.get('deadlineDateTimeStamp');
+        deadlineDate = jobDatabase.get('deadlineDate');
+
+        var postDate = postedDateTimeStamp!.toDate();
+
+        postedDate = '${postDate.year}-${postDate.month}-${postDate.day}';
+
+      });
+
+      var date = deadlineDateTimeStamp!.toDate();
+      isDeadlineAvailable = date.isAfter(DateTime.now());
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.green.shade200, Colors.yellowAccent.shade100],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: const [0.2, 0.9],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.yellowAccent.shade100, Colors.green.shade200],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                stops: const [0.2, 0.9],
+              ),
+            ),
+          ),
+          leading: IconButton(
+            onPressed: (){
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const JobScreen()));
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_outlined,
+              color: Colors.black,
+              size: 40,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
